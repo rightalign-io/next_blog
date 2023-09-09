000113f387fa13c660177ac735043a443c23092a
Vukani Gcabashe <gcabashevukani3@gmail.com>
2023-09-09 02:41:28 -1200

    set auto for width

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -1,7 +1,7 @@
 
 const Trending = () => {
     const trendintItems = [{title: 'trend 1', author: 'Author 1'} ,{title: 'trend 2', author: 'Author 2'} ,{title: 'trend 3', author: 'Author3 '} , {title: 'trend 4', author: 'Author 4'}, {title: 'trend 5', author: 'Author 5'}];
-    return <div className="w-1/5 md:">
+    return <div className="md:w-fit ">
       <div className="trending">
         <h3>Trending</h3>
         <ul className="trending-post">
